21aad44352c0670d4d5dbd290a59f83796627794
Boris Rybalkin <ribalkin@gmail.com>
2020-03-03 21:33:37 +0000

    fix screeshots

--- integration/test-ui.py
+++ integration/test-ui.py
@@ -16,17 +16,28 @@ from syncloudlib.integration.screenshots import screenshots
 
 
 DIR = dirname(__file__)
-screenshot_dir = join(DIR, 'screenshot')
 
-def test_start(app, device_host):
-    if exists(screenshot_dir):
-        shutil.rmtree(screenshot_dir)
-    os.mkdir(screenshot_dir)
 
-    add_host_alias(app, device_host)
+@pytest.fixture(scope="session")
+def module_setup(request, device, log_dir, ui_mode, artifact_dir):
+    def module_teardown():
+        tmp_dir = '/tmp/syncloud/ui'
+        device.activated()
+        device.run_ssh('mkdir -p {0}'.format(tmp_dir), throw=False)
+        device.run_ssh('journalctl > {0}/journalctl.ui.{1}.log'.format(tmp_dir, ui_mode), throw=False)
+        device.run_ssh('cp /var/log/syslog {0}/syslog.ui.{1}.log'.format(tmp_dir, ui_mode), throw=False)
+      
+        device.scp_from_device('{0}/*'.format(tmp_dir), artifact_dir)
+        check_output('chmod -R a+r {0}'.format(artifact_dir), shell=True)
+
+    request.addfinalizer(module_teardown)
+
+
+def test_start(module_setup, app, domain, device_host):
+    add_host_alias_by_ip(app, domain, device_host)
     
 
-def test_index(driver, app_domain):
+def test_index(driver, app_domain, screenshot_dir):
 
     driver.get("https://{0}".format(app_domain))
     time.sleep(10)
@@ -34,7 +45,7 @@ def test_index(driver, app_domain):
     screenshots(driver, screenshot_dir, 'index')
     
 
-def test_login(driver, app_domain, device_user, device_password):
+def test_login(driver, app_domain, device_user, device_password, screenshot_dir):
 
     driver.get("https://{0}/wp-login.php".format(app_domain))
     wait_driver = WebDriverWait(driver, 120)
@@ -52,34 +63,34 @@ def test_login(driver, app_domain, device_user, device_password):
     screenshots(driver, screenshot_dir, 'login-complete')
     
 
-def test_admin(driver, app_domain):
+def test_admin(driver, app_domain, screenshot_dir):
 
     driver.get("https://{0}/wp-admin".format(app_domain))
     time.sleep(10)
     screenshots(driver, screenshot_dir, 'admin')
     
 
-def test_profile(driver, app_domain):
+def test_profile(driver, app_domain, screenshot_dir):
 
     driver.get("https://{0}/wp-admin/profile.php".format(app_domain))
     time.sleep(10)
     screenshots(driver, screenshot_dir, 'profile')
     
 
-def test_ldap(driver, app_domain):
+def test_ldap(driver, app_domain, screenshot_dir):
 
     driver.get("https://{0}/wp-admin/admin.php?page=mo_ldap_local_login".format(app_domain))
     time.sleep(10)
     screenshots(driver, screenshot_dir, 'ldap')
 
     
-def test_users(driver, app_domain):
+def test_users(driver, app_domain, screenshot_dir):
 
     driver.get("https://{0}/wp-admin/users.php".format(app_domain))
     time.sleep(10)
     screenshots(driver, screenshot_dir, 'users')
     
-def test_media(driver, app_domain):
+def test_media(driver, app_domain, screenshot_dir):
 
     driver.get("https://{0}/wp-admin/media-new.php".format(app_domain))
     time.sleep(2)
