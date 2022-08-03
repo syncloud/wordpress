12909c2971fd0a4ad66c52624eb73482f2f796fe
Boris Rybalkin <ribalkin@gmail.com>
2022-08-03 18:57:51 +0300

    fix

--- bin/service.nginx.sh
+++ bin/service.nginx.sh
@@ -13,7 +13,7 @@ pre-start)
     exec ${DIR}/nginx/sbin/nginx -t -c /var/snap/wordpress/current/config/nginx.conf -p ${DIR}/nginx -g 'error_log '${SNAP_COMMON}'/log/nginx_error.log warn;'
     ;;
 start)
-    exec ${DIR}/nginx/sbin/nginx -c ${SNAP_COMMON}/config/nginx.conf -p ${DIR}/nginx -g 'error_log '${SNAP_COMMON}'/log/nginx_error.log warn;'
+    exec ${DIR}/nginx/sbin/nginx -c /var/snap/wordpress/current/config/nginx.conf -p ${DIR}/nginx -g 'error_log '${SNAP_COMMON}'/log/nginx_error.log warn;'
     ;;
 post-start)
     timeout 5 /bin/bash -c 'until [ -S '${SNAP_COMMON}'/web.socket ]; do echo "waiting for ${SNAP_COMMON}/web.socket"; sleep 1; done'
