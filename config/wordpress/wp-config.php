d42e5439f8537d0e16a22ad60c3ec07fbb52d45a
Boris Rybalkin <ribalkin@gmail.com>
2023-08-04 17:43:05 +0100

    fix ABSPATH

--- config/wordpress/wp-config.php
+++ config/wordpress/wp-config.php
@@ -83,7 +83,7 @@ define('WP_DEBUG', false);
 
 /** Absolute path to the WordPress directory. */
 if ( !defined('ABSPATH') )
-	define('ABSPATH', getenv('SNAP') . '/wordpress');
+	define('ABSPATH', getenv('SNAP') . '/php/wordpress');
 
 require_once(ABSPATH . 'wp-settings.php');
 
