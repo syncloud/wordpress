455cb58c0df7f9e66f1d756e765fbe2785322cbf
Boris Rybalkin <ribalkin@gmail.com>
2025-12-20 09:54:00 +0000

    fix package

--- package.sh
+++ package.sh
@@ -1,7 +1,6 @@
-#!/bin/bash -ex
+#!/bin/bash -xe
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-cd ${DIR}
 
 if [[ -z "$2" ]]; then
     echo "usage $0 app version"
@@ -11,26 +10,26 @@ fi
 NAME=$1
 VERSION=$2
 ARCH=$(dpkg --print-architecture)
-
 SNAP_DIR=${DIR}/build/snap
 
 apt update
-apt -y install squashfs-tools wget
+apt -y install squashfs-tools
 
-cp -r ${DIR}/bin ${SNAP_DIR}
+cp -r ${DIR}/bin/* ${SNAP_DIR}/bin
 cp -r ${DIR}/config ${SNAP_DIR}
-cp ${DIR}/snap.yaml ${SNAP_DIR}/meta
+cp -r ${DIR}/hooks ${SNAP_DIR}
+cp -r ${DIR}/meta ${SNAP_DIR}
+
+mkdir ${SNAP_DIR}/META
+echo ${NAME} >> ${SNAP_DIR}/META/app
+echo ${VERSION} >> ${SNAP_DIR}/META/version
 
 echo "version: $VERSION" >> ${SNAP_DIR}/meta/snap.yaml
 echo "architectures:" >> ${SNAP_DIR}/meta/snap.yaml
 echo "- ${ARCH}" >> ${SNAP_DIR}/meta/snap.yaml
-echo $VERSION > ${SNAP_DIR}/version
-
-du -d10 -h $SNAP_DIR | sort -h | tail -100
 
 PACKAGE=${NAME}_${VERSION}_${ARCH}.snap
 echo ${PACKAGE} > ${DIR}/package.name
 mksquashfs ${SNAP_DIR} ${DIR}/${PACKAGE} -noappend -comp xz -no-xattrs -all-root
 mkdir ${DIR}/artifact
 cp ${DIR}/${PACKAGE} ${DIR}/artifact
-
