c49b985d663474efed6761a520061feb27486b83
Boris Rybalkin <ribalkin@gmail.com>
2025-12-20 18:29:24 +0000

    nginx fix

--- nginx/bin/nginx.sh
+++ nginx/bin/nginx.sh
@@ -1,5 +1,4 @@
 #!/bin/bash -e
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
-LIBS=$(echo ${DIR}/lib/*-linux-gnu*)
-LIBS=$LIBS:$(echo ${DIR}/usr/lib/*-linux-gnu*)
-${DIR}/lib/*-linux*/ld-*.so --library-path $LIBS ${DIR}/usr/sbin/nginx "$@"
+LIBS=${DIR}/usr/lib
+${DIR}/lib/ld-*.so* --library-path $LIBS ${DIR}/usr/sbin/nginx "$@"
