fd4188aa2d22576527b5ee77e1c6dac60144cf89
Boris Rybalkin <ribalkin@gmail.com>
2025-12-22 18:28:03 +0000

    retry index

--- test/ui.py
+++ test/ui.py
@@ -29,12 +29,13 @@ def test_start(module_setup, app, domain, device_host):
     add_host_alias(app, device_host, domain)
 
 
-pytest.mark.flaky(retries=10, delay=5)
+@pytest.mark.flaky(retries=10, delay=5)
 def test_visible_through_platform(app_domain):
     response = requests.get('https://{0}'.format(app_domain), verify=False)
     assert response.status_code == 200, response.text
 
 
+@pytest.mark.flaky(retries=10, delay=5)
 def test_index(selenium):
     selenium.open_app()
     selenium.find_by_xpath("//a[text()='Syncloud']")
