626e9b3ced0fa2f86a02c8788ef5c218735e8e47
Boris Rybalkin <ribalkin@gmail.com>
2025-12-26 16:37:42 +0000

    no iframe

--- test/lib.py
+++ test/lib.py
@@ -26,12 +26,12 @@ def post_prev(selenium, first_time):
     if first_time:
         selenium.click_by(By.XPATH, '//button[@aria-label="Close"]')
     selenium.screenshot('post-prev')
-    selenium.driver.switch_to.frame("editor-canvas")
+    #selenium.driver.switch_to.frame("editor-canvas")
     title = selenium.find_by(By.XPATH, "//h1[contains(@class, 'wp-block-post-title')]")
     title.send_keys("syncloud title")
     paragraph = selenium.find_by(By.XPATH, "//p[@aria-label='Add default block']")
     paragraph.send_keys("syncloud paragraph")
-    selenium.driver.switch_to.default_content()
+    #selenium.driver.switch_to.default_content()
 
     selenium.click_by(By.XPATH, "//button[.='Publish']")
     selenium.click_by(By.XPATH, "//button[.='Cancel']/../..//button[.='Publish']")
